e7705c91fae750afb23e5c1784baba0b78c8e0c8
Samidius (VoroniN) <78709877+Samidius-mag@users.noreply.github.com>
2023-05-26 07:54:03 +1000

    Update load.js

--- load.js
+++ load.js
@@ -1,52 +1,53 @@
-const fetch = require('node-fetch');
-const fs = require('fs');
 const csv = require('csv-parser');
 const createCsvWriter = require('csv-writer').createObjectCsvWriter;
 
 // Получаем данные о последней часовой свече инструмента BTC\USDT с сайта api.binance.com
-fetch('https://api.binance.com/api/v3/klines?symbol=BTCUSDT&interval=1h&limit=1')
-  .then(response => response.json())
-  .then(data => {
-    // Разбираем полученные данные и извлекаем необходимые значения
-    const timestamp = parseInt(data[0][0]) / 1000;
-    const date = new Date(timestamp).toISOString().slice(0, 10);
-    const time = new Date(timestamp).toISOString().slice(11, 19);
-    const openPrice = parseFloat(data[0][1]);
-    const highPrice = parseFloat(data[0][2]);
-    const lowPrice = parseFloat(data[0][3]);
-    const closePrice = parseFloat(data[0][4]);
-    const volume = parseFloat(data[0][5]);
+(async () => {
+  const response = await import('node-fetch');
+  const fetch = response.default;
+  const data = await fetch('https://api.binance.com/api/v3/klines?symbol=BTCUSDT&interval=1h&limit=1')
+    .then(response => response.json());
 
-    // Добавляем полученные значения в конец файла price.csv
-    const csvWriter = createCsvWriter({
-      path: 'price.csv',
-      header: [
-        {id: 'date', title: 'DATE'},
-        {id: 'time', title: 'TIME'},
-        {id: 'open', title: 'OPEN'},
-        {id: 'high', title: 'HIGH'},
-        {id: 'low', title: 'LOW'},
-        {id: 'close', title: 'CLOSE'},
-        {id: 'vol', title: 'VOL'}
-      ],
-      append: true
-    });
+  // Разбираем полученные данные и извлекаем необходимые значения
+  const timestamp = parseInt(data[0][0]) / 1000;
+  const date = new Date(timestamp).toISOString().slice(0, 10);
+  const time = new Date(timestamp).toISOString().slice(11, 19);
+  const openPrice = parseFloat(data[0][1]);
+  const highPrice = parseFloat(data[0][2]);
+  const lowPrice = parseFloat(data[0][3]);
+  const closePrice = parseFloat(data[0][4]);
+  const volume = parseFloat(data[0][5]);
 
-    const record = {
-      date: date,
-      time: time,
-      open: openPrice,
-      high: highPrice,
-      low: lowPrice,
-      close: closePrice,
-      vol: volume
-    };
-
-    csvWriter.writeRecords([record])
-      .then(() => {
-        console.log('Данные успешно добавлены в файл price.csv');
-      });
-  })
-  .catch(error => {
-    console.error('Ошибка:', error);
+  // Добавляем полученные значения в конец файла price.csv
+  const csvWriter = createCsvWriter({
+    path: 'price.csv',
+    header: [
+      {id: 'date', title: 'DATE'},
+      {id: 'time', title: 'TIME'},
+      {id: 'open', title: 'OPEN'},
+      {id: 'high', title: 'HIGH'},
+      {id: 'low', title: 'LOW'},
+      {id: 'close', title: 'CLOSE'},
+      {id: 'vol', title: 'VOL'}
+    ],
+    append: true
   });
+
+  const record = {
+    date: date,
+    time: time,
+    open: openPrice,
+    high: highPrice,
+    low: lowPrice,
+    close: closePrice,
+    vol: volume
+  };
+
+  csvWriter.writeRecords([record])
+    .then(() => {
+      console.log('Данные успешно добавлены в файл price.csv');
+    })
+    .catch(error => {
+      console.error('Ошибка при записи в файл:', error);
+    });
+})();
