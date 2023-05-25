63e908ac0214cc181cb2c01d93ec11711e36d862
Samidius (VoroniN) <78709877+Samidius-mag@users.noreply.github.com>
2023-05-26 09:31:43 +1000

    Update load.js

--- load.js
+++ load.js
@@ -1,37 +1,50 @@
-const request = require('request');
+const axios = require('axios');
 const fs = require('fs');
 
-const url = 'https://api.binance.com/api/v3/klines?symbol=BTCUSDT&interval=1h&limit=1';
+const FILE_NAME = 'price.json';
+const SYMBOL = 'BTCUSDT';
 
-request(url, { json: true }, (err, res, body) => {
-  if (err) { return console.log(err); }
+// Функция для форматирования даты и времени
+function formatDate(date) {
+  const year = date.getFullYear();
+  const month = ('0' + (date.getMonth() + 1)).slice(-2);
+  const day = ('0' + date.getDate()).slice(-2);
+  const hours = ('0' + date.getHours()).slice(-2);
+  const minutes = ('0' + date.getMinutes()).slice(-2);
+  const seconds = ('0' + date.getSeconds()).slice(-2);
+  return `${year}${month}${day},${hours}${minutes}${seconds}`;
+}
 
-  const [time, open, high, low, close, vol] = body[0];
-  const date = new Date(time);
+// Функция для добавления данных в файл
+function appendToFile(data) {
+  fs.appendFileSync(FILE_NAME, data + '\n');
+}
 
-  const price = {
-    DATE: date.getFullYear() + ('0' + (date.getMonth() + 1)).slice(-2) + ('0' + date.getDate()).slice(-2),
-    TIME: ('0' + date.getHours()).slice(-2) + ('0' + date.getMinutes()).slice(-2),
-    OPEN: open,
-    HIGH: high,
-    LOW: low,
-    CLOSE: close,
-    VOL: vol
+// Функция для получения последней свечи
+async function getLastCandle(symbol) {
+  const response = await axios.get(`https://api.binance.com/api/v3/klines?symbol=${symbol}&interval=1h&limit=1`);
+  const [time, open, high, low, close, volume] = response.data[0];
+  return {
+    date: formatDate(new Date(time)),
+    open,
+    high,
+    low,
+    close,
+    volume
   };
+}
 
-  fs.readFile('price.json', 'utf8', (err, data) => {
-    if (err) {
-      fs.writeFile('price.json', JSON.stringify([price]), 'utf8', (err) => {
-        if (err) { return console.log(err); }
-        console.log('Data added to the beginning of the file.');
-      });
-    } else {
-      const prices = JSON.parse(data);
-      prices.push(price);
-      fs.writeFile('price.json', JSON.stringify(prices), 'utf8', (err) => {
-        if (err) { return console.log(err); }
-        console.log('Data added to the end of the file.');
-      });
+// Основной код
+(async function() {
+  try {
+    const lastCandle = await getLastCandle(SYMBOL);
+    const fileExists = fs.existsSync(FILE_NAME);
+    if (!fileExists) {
+      appendToFile('DATE,TIME,OPEN,HIGH,LOW,CLOSE,VOL');
     }
-  });
-});
+    appendToFile(`${lastCandle.date},${lastCandle.open},${lastCandle.high},${lastCandle.low},${lastCandle.close},${lastCandle.volume}`);
+    console.log('Данные успешно добавлены в файл');
+  } catch (error) {
+    console.error(error);
+  }
+})();
