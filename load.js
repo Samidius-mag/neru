adc6512f12b711948044a2e7f50b893c9a2e098b
Samidius (VoroniN) <78709877+Samidius-mag@users.noreply.github.com>
2023-05-26 09:28:39 +1000

    Update load.js

--- load.js
+++ load.js
@@ -1,66 +1,37 @@
-const https = require('https');
+const request = require('request');
 const fs = require('fs');
 
-// Функция для выполнения запроса к API Binance
-function getPriceData(callback) {
-  https.get('https://api.binance.com/api/v3/klines?symbol=BTCUSDT&interval=1h&limit=1', (res) => {
-    let data = '';
-    res.on('data', (chunk) => {
-      data += chunk;
-    });
-    res.on('end', () => {
-      callback(JSON.parse(data));
-    });
-  }).on('error', (err) => {
-    console.log('Error: ' + err.message);
-  });
-}
+const url = 'https://api.binance.com/api/v3/klines?symbol=BTCUSDT&interval=1h&limit=1';
 
-// Функция для добавления данных в файл price.json
-function appendPriceData(data) {
-  fs.readFile('price.json', 'utf8', (err, fileData) => {
+request(url, { json: true }, (err, res, body) => {
+  if (err) { return console.log(err); }
+
+  const [time, open, high, low, close, vol] = body[0];
+  const date = new Date(time);
+
+  const price = {
+    DATE: date.getFullYear() + ('0' + (date.getMonth() + 1)).slice(-2) + ('0' + date.getDate()).slice(-2),
+    TIME: ('0' + date.getHours()).slice(-2) + ('0' + date.getMinutes()).slice(-2),
+    OPEN: open,
+    HIGH: high,
+    LOW: low,
+    CLOSE: close,
+    VOL: vol
+  };
+
+  fs.readFile('price.json', 'utf8', (err, data) => {
     if (err) {
-      console.log('Error: ' + err.message);
-      return;
+      fs.writeFile('price.json', JSON.stringify([price]), 'utf8', (err) => {
+        if (err) { return console.log(err); }
+        console.log('Data added to the beginning of the file.');
+      });
+    } else {
+      const prices = JSON.parse(data);
+      prices.push(price);
+      fs.writeFile('price.json', JSON.stringify(prices), 'utf8', (err) => {
+        if (err) { return console.log(err); }
+        console.log('Data added to the end of the file.');
+      });
     }
-    let prices = [];
-    if (fileData) {
-      prices = JSON.parse(fileData);
-    }
-    
-    const lastPrice = data[0];
-    const date = new Date(lastPrice[0]);
-    const dateString = date.getFullYear() + pad(date.getMonth() + 1) + pad(date.getDate());
-    const timeString = pad(date.getHours()) + pad(date.getMinutes()) + pad(date.getSeconds());
-    const open = lastPrice[1];
-    const high = lastPrice[2];
-    const low = lastPrice[3];
-    const close = lastPrice[4];
-    const volume = lastPrice[5];
-    const newPrice = {
-      DATE: dateString,
-      TIME: timeString,
-      OPEN: open,
-      HIGH: high,
-      LOW: low,
-      CLOSE: close,
-      VOL: volume
-    };
-    prices.push(newPrice);
-    fs.writeFile('price.json', JSON.stringify(prices), (err) => {
-      if (err) {
-        console.log('Error: ' + err.message);
-        return;
-      }
-      console.log('Data appended to price.json');
-    });
   });
-}
-
-// Функция для добавления ведущего нуля к числу, если оно меньше 10
-function pad(num) {
-  return num < 10 ? '0' + num : num;
-}
-
-// Вызываем функцию для получения данных о последней свече
-getPriceData(appendPriceData);
+});
